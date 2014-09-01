2ae79e577c94512467bc093d8b07bf7fe8fb52e4
Laura Baakman <laurabaakman@gmail.com>
2014-09-01 16:20:20 +0200

    Voldoet nu aan peps.

--- practicum0/OpenGL.py
+++ practicum0/OpenGL.py
@@ -9,82 +9,85 @@ H.Bekker@rug.nl
 
 from sys import *
 from random import *
-from math import sqrt
 import operator
 
 
 try:
-  from OpenGL.GLUT import *
-  from OpenGL.GL import *
-  from OpenGL.GLU import *
+    from OpenGL.GLUT import *
+    from OpenGL.GL import *
+    from OpenGL.GLU import *
 except:
-  print '''ERROR: PyOpenGL not installed properly.'''
-  print '''Go get it: http://atrpms.net/'''
-  exit ( 2 )
+    print 'ERROR: PyOpenGL not installed properly.'
+    print '''Go get it: http://atrpms.net/'''
+    exit(2)
 
 # globals
-points=[]  # points
-spoints=[] # x_sorted points
-width  = 800  # screen x_size
+points = []  # points
+spoints = []  # x_sorted points
+width = 800  # screen x_size
 height = 800  # screen y_size
 seed(5)  # seed value of random
 
 
 def generate_points():
-   for i in range(60):
-      x=random()
-      y=random()
-      r=(x*x+y*y)
-      if r>0.5 and r<1.0 :
-         points.append([500*x+100, 500*y+100])
-
-
+    """Generate 60 points."""
+    for i in range(60):
+        x = random()
+        y = random()
+        r = (x * x + y * y)
+        if r > 0.5 and r < 1.0:
+            points.append([500 * x + 100, 500 * y + 100])
 
 
 def display():
-    glClear (GL_COLOR_BUFFER_BIT)
+    """OpenGL display function."""
+    glClear(GL_COLOR_BUFFER_BIT)
     # Draw points
-    glColor3f (1.0, 1.0, 1.0)
-    glPointSize (3)
+    glColor3f(1.0, 1.0, 1.0)
+    glPointSize(3)
     glBegin(GL_POINTS)
     for p in points:
-       glVertex2f (p [0], p [1])
+        glVertex2f(p[0], p[1])
     glEnd()
-    glColor3f (1.0, 0.0, 0.0)
+    glColor3f(1.0, 0.0, 0.0)
     # Draw lines
-    glBegin (GL_LINES)
-    for i in range(len(spoints)-1):
-       glVertex2f (spoints[i][0],spoints[i][1])
-       glVertex2f (spoints[i+1][0], spoints[i+1][1])
+    glBegin(GL_LINES)
+    for i in range(len(spoints) - 1):
+        glVertex2f(spoints[i][0], spoints[i][1])
+        glVertex2f(spoints[i + 1][0], spoints[i + 1][1])
     glEnd()
-    glutSwapBuffers ()  # display
+    glutSwapBuffers()  # display
 
-def reshape (wid, hgt):
+
+def reshape(wid, hgt):
+    """Fire when the window is reshaped."""
     global width, height
     width = wid
     height = hgt
-    glViewport( 0, 0, width, height )
-    glMatrixMode (GL_PROJECTION)
-    glLoadIdentity ()
-    gluOrtho2D (0, width, height, 0) # reshape
+    glViewport(0, 0, width, height)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluOrtho2D(0, width, height, 0)  # reshape
 
-def main (argv=None):
+
+def main(argv=None):
+    """Main."""
     if argv is None:
-       argv = sys.argv
+        argv = sys.argv
     generate_points()
     global spoints
-    spoints=sorted(points, key=operator.itemgetter(0))  # sort points in x direction
+    spoints = sorted(points, key=operator.itemgetter(0))  # sort points in x direction
     glutInit(argv)
-    glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB)
-    glutInitWindowSize (width, height)
-    glutInitWindowPosition (100, 100)
-    glutCreateWindow ("Example1")
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB)
+    glutInitWindowSize(width, height)
+    glutInitWindowPosition(100, 100)
+    glutCreateWindow("Example1")
     glutDisplayFunc(display)
     glutIdleFunc(display)
-    glutReshapeFunc (reshape)
+    glutReshapeFunc(reshape)
     glutMainLoop()
     return
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     sys.exit(main())
