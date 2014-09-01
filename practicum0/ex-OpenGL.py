e05d776f663dfdc1abd94979ef78b936d5ab2fa5
Laura Baakman <laurabaakman@gmail.com>
2014-09-01 17:23:33 +0200

    Rename en commentaar toegevoegd.

--- practicum0/ex-OpenGL.py
+++ practicum0/ex-OpenGL.py
@@ -1,7 +1,6 @@
 """
-Example of OpenGL with python.
-
 This example is given to get started with python and opengl in python.
+
 Some random points are generated, sorted, connected by line segments and visualized.
 H.Bekker@rug.nl
 """
@@ -17,7 +16,7 @@ try:
     from OpenGL.GL import *
     from OpenGL.GLU import *
 except:
-    print 'ERROR: PyOpenGL not installed properly.'
+    print '''ERROR: PyOpenGL not installed properly.'''
     print '''Go get it: http://atrpms.net/'''
     exit(2)
 
@@ -30,7 +29,7 @@ seed(5)  # seed value of random
 
 
 def generate_points():
-    """Generate 60 points."""
+    """Generate points."""
     for i in range(60):
         x = random()
         y = random()
@@ -42,15 +41,24 @@ def generate_points():
 def display():
     """OpenGL display function."""
     glClear(GL_COLOR_BUFFER_BIT)
+
     # Draw points
-    glColor3f(1.0, 1.0, 1.0)
+    # Set the colour of the points to green
+    glColor3f(0.0, 1.0, 0.0)
+
+    # Set the size of the points
     glPointSize(3)
+
+    # Draw the points
     glBegin(GL_POINTS)
     for p in points:
         glVertex2f(p[0], p[1])
     glEnd()
-    glColor3f(1.0, 0.0, 0.0)
+
     # Draw lines
+    # Set the colours of the lines to red
+    glColor3f(1.0, 0.0, 0.0)
+
     glBegin(GL_LINES)
     for i in range(len(spoints) - 1):
         glVertex2f(spoints[i][0], spoints[i][1])
@@ -60,23 +68,25 @@ def display():
 
 
 def reshape(wid, hgt):
-    """Fire when the window is reshaped."""
+    """OpenGL reshape function."""
     global width, height
     width = wid
     height = hgt
     glViewport(0, 0, width, height)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    gluOrtho2D(0, width, height, 0)  # reshape
+    # reshape
+    gluOrtho2D(0, width, height, 0)
 
 
 def main(argv=None):
-    """Main."""
+    """Main function."""
     if argv is None:
         argv = sys.argv
     generate_points()
     global spoints
-    spoints = sorted(points, key=operator.itemgetter(0))  # sort points in x direction
+    # sort points in x direction
+    spoints = sorted(points, key=operator.itemgetter(0))
     glutInit(argv)
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB)
     glutInitWindowSize(width, height)
