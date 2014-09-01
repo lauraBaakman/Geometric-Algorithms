6863f513833683becb95df855a768270929293f1
Laura Baakman <laurabaakman@gmail.com>
2014-09-01 15:58:13 +0200

    Formatting aangepast zodat die voldoet aan pep8 en pep257 en de idiomatic way van file io volgens Knupp toegevoegd.

--- practicum0/fileio.py
+++ practicum0/fileio.py
@@ -1,23 +1,34 @@
-#!/usr/bin/env python
+"""Example of doing file IO in python."""
 
-import random
-import string
+from random import seed, random
+from string import split
 
 n = int(raw_input("Please enter an integer: "))
 fout = open("output.dat", "w")
-random.seed(5)  # seed value of random generator
+seed(5)  # seed value of random generator
 for i in range(n):
-  x=random.random()
-  y=random.random()
-  print 'x y =', x , y
-  fout.write(str(x) + " " + str(y)  + "\n")
+    x = random()
+    y = random()
+    print 'x y =', x, y
+    fout.write(str(x) + " " + str(y) + "\n")
 fout.close()
+
 print ""
-fin=open("output.dat",'r')
+
+fin = open("output.dat", 'r')
 for line in fin:
-   b=string.split(line)
-   x=(float(b[0]))
-   y=(float(b[1]))
-   print 'x y =', x , y
+    b = split(line)
+    x = (float(b[0]))
+    y = (float(b[1]))
+    print 'x y =', x, y
 fin.close()
- 
+
+print ""
+
+# Pythonic way according to Knupp:
+with open('output.dat', 'r') as file_handle:
+    for line in file_handle:
+        b = split(line)
+        x = (float(b[0]))
+        y = (float(b[1]))
+        print("x y = {x}, {y}".format(x=x, y=y))
