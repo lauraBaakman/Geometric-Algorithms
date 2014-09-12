2b9ccf75e4a73a5063c3a87def2524ea6a0c4b9f
Laura Baakman <laurabaakman@gmail.com>
2014-09-12 13:25:50 +0200

    Verslag assignment D is klaar.

--- practicum1/assignment1D.py
+++ practicum1/assignment1D.py
@@ -79,20 +79,6 @@ class Line(object):
         """Check if two objects are not equal."""
         return not self.__eq__(other)
 
-    def __lt__(self, other):
-        if self.a == other.b:
-            return self.b <= other.b
-        else:
-            return self.a <= other.a
-# a = Fraction(24, 10)
-# b = Fraction(24987654321345612349056, 113570864213680646852690)
-# pi = Fraction.from_float(3.14159265358979)
-# c = Fraction.from_float(random())
-# d = Fraction.from_float(random())
-# tmp = (((a + b) * pi - c / d) ** 2)
-# print tmp
-# print float(tmp)
-
 
 def testLinePair(line1, line2):
     """
