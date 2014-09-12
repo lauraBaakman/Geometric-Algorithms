ff097ad64daf0e055f2f304eb532afbf2a098be6
Laura Baakman <laurabaakman@gmail.com>
2014-09-12 12:55:34 +0200

    Code van assignment D is klaar.

--- practicum1/assignment1D.py
+++ practicum1/assignment1D.py
@@ -2,6 +2,7 @@
 
 from fractions import *
 from random import uniform
+from collections import Counter
 
 
 class Line(object):
@@ -42,15 +43,20 @@ class Line(object):
             Fraction.from_float(uniform(minimum, maximum))
         )
 
-    def pointOnLine(self, p):
-        """Determine if the point p is on this line."""
-        pass
+    def computeYCoordinate(self, x):
+        """Compute the y-coordinate of a point on the line, given an x-coordinate."""
+        return self.a * x + self.b
+
+    def pointOnLine(self, (x, y)):
+        """Determine if the point p = (x,y) lies on this line."""
+        result = self.a * x + self.b - y
+        return not result
 
     def intersectionPoint(self, l):
         """Determine at which point, if any, this line intersects line l."""
         try:
             x = (-self.b + l.b) / (self.a - l.a)
-            y = self.a * x + self.b
+            y = self.computeYCoordinate(x)
             return (x, y)
         except ZeroDivisionError:
             return None
@@ -73,7 +79,11 @@ class Line(object):
         """Check if two objects are not equal."""
         return not self.__eq__(other)
 
-
+    def __lt__(self, other):
+        if self.a == other.b:
+            return self.b <= other.b
+        else:
+            return self.a <= other.a
 # a = Fraction(24, 10)
 # b = Fraction(24987654321345612349056, 113570864213680646852690)
 # pi = Fraction.from_float(3.14159265358979)
@@ -83,11 +93,60 @@ class Line(object):
 # print tmp
 # print float(tmp)
 
+
+def testLinePair(line1, line2):
+    """
+    Test if the intersection of line1 and line2 lies on both lines.
+
+    Returns true if the intersectionPoint lies on both lines, false
+    if it does not and None if there was no intersection.
+    """
+    intersection = line1.intersectionPoint(line2)
+    if intersection:
+        return (
+            line1.pointOnLine(intersection) and
+            line2.pointOnLine(intersection)
+        )
+    else:
+        return None
+
+
+def test(generationMethod, N=100):
+    """
+    Run the test that is part of this assignment.
+
+    Args:
+        generationMethod: The factory method of Line to be used.
+        N: The number of lines to be generated, defaults to 100
+    """
+    lines = [generationMethod() for x in range(N)]
+
+    results = []
+    for idx, line1 in enumerate(lines):
+        for line2 in lines[idx + 1:]:
+            results.append(testLinePair(line1, line2))
+    return Counter(results)
+
+
 def main():
     """Main method."""
-    l1 = Line(2, 3)
-    l2 = Line(2, 7)
-    print(l1.intersectionPoint(l2))
+    floatResult = test(Line.randomWithFloat)
+    fractionResult = test(Line.randomWithFraction)
+    resultString = "Using {type} {trues} intersections coincided with"\
+        "both lines, and {falses} didn't. {nones} pairs of lines had "\
+        "no intersection."
+    print resultString.format(
+        type='floats',
+        trues=floatResult[True],
+        falses=floatResult[False],
+        nones=floatResult[None],
+    )
+    print resultString.format(
+        type='fractions',
+        trues=fractionResult[True],
+        falses=fractionResult[False],
+        nones=fractionResult[None],
+    )
 
 
 if __name__ == '__main__':
