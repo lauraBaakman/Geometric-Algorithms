32d2990199d5539f8cf35c4ac012ee171c5de83e
Laura Baakman <laurabaakman@gmail.com>
2014-09-19 16:58:33 +0200

    Vectors uitgebreid met optellen van vectoren en vermenigvuldigen met een scalar.

--- practicum2/vectors.py
+++ practicum2/vectors.py
@@ -6,8 +6,23 @@ def substract(v1, v2):
     return [v1_i - v2_i for v1_i, v2_i in zip(v1, v2)]
 
 
+def add(v1, v2):
+    """Add vector v1 to vector v2."""
+    return [v1_i + v2_i for v1_i, v2_i in zip(v1, v2)]
+
+
+def multiplicate(v1, s):
+    """Multiplicate vector v1 with scalar s."""
+    return [v1_i * s for v1_i in v1]
+
+
+def divide(v, s):
+    """Divide the elements of the vector v1 by the scalar s."""
+    return [(float(v_i) / s) for v_i in v]
+
+
 def cross(v1, v2):
-    """Compute the cross product v1 x v2."""
+    """Compute the cross product of 3D vectors v1 and v2 as: v1 x v2."""
     return([
         -(v1[2] * v2[1]) + v1[1] * v2[2],
         v1[2] * v2[0] - v1[0] * v2[2],
@@ -20,8 +35,3 @@ def cross_2d_as_3d(v1, v2):
     v1.append(0)
     v2.append(0)
     return cross(v1, v2)
-
-if __name__ == '__main__':
-    v1 = [1, 2]
-    v2 = [3, 4]
-    print(cross_2d_as_3d(v1, v2))
