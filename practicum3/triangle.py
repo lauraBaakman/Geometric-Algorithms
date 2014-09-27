c5db58e48ca03a46660b611beca6cc4f9b73a854
Laura Baakman <laurabaakman@gmail.com>
2014-09-27 08:46:15 +0200

    Paar fouten gefixt.

--- practicum3/triangle.py
+++ practicum3/triangle.py
@@ -1,4 +1,6 @@
 """Module with methods that handle things related to triangles."""
+from __future__ import division
+import pdb
 
 
 def point_in_triangle(triangle, point):
@@ -11,26 +13,22 @@ def point_in_triangle(triangle, point):
         point: List with the x and y coordinate of the point.
     """
     [p1, p2, p3] = triangle
-    a_denominator = (
-        p1[1] * p2[0] - p1[0] * p2[1] - p1[1] * p3[0] +
-        p2[1] * p3[0] + p1[0] * p3[1] - p2[0] * p3[1]
+    v1_cross_v2 = (
+        -p1[1] * p2[0] + p1[0] * p2[1] + p1[1] * p3[0]
+        - p2[1] * p3[0] - p1[0] * p3[1] + p2[0] * p3[1]
     )
-    if(a_denominator):
+    # pdb.set_trace()
+    if(v1_cross_v2):
         a_numerator = (
             p1[1] * p3[0] - p1[0] * p3[1] - p1[1] * point[0] +
             p3[1] * point[0] + p1[0] * point[1] - p3[0] * point[1]
         )
-        a = a_numerator / a_denominator
+        a = a_numerator / v1_cross_v2
         if(a > 0 and a < 1):
-            b_denominator = (
-                -(p1[1] * p2[0]) + p1[0] * p2[1] + p1[1] * p3[0]
-                - p2[1] * p3[0] - p1[0] * p3[1] + p2[0] * p3[1]
+            b_numerator = (
+                p1[1] * p2[0] - p1[0] * p2[1] - p1[1] * point[0] +
+                p2[1] * point[0] + p1[0] * point[1] - p2[0] * point[1]
             )
-            if(b_denominator):
-                b_numerator = (
-                    p1[1] * p2[0] - p1[0] * p2[1] - p1[1] * point[0] +
-                    p2[1] * point[0] + p1[0] * point[1] - p2[0] * point[1]
-                )
-                b = b_numerator / b_denominator
-                return (a + b < 1)
+            b = - b_numerator / v1_cross_v2
+            return (b > 0 and b < 1) and (a + b < 1)
     return False
